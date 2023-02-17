3bf05f5fc7a19eea1aa3e33d31c165b0057342c0
Matheus Silva <ms25022003@gmail.com>
2023-02-17 10:42:54 -0300

    style: circle-spin-icon

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,5 @@
 import { MouseEvent } from "react";
-
+import CircleSpin from "assets/circle-spin-icon.svg";
 interface ModalProps {
   onClick: (e: MouseEvent<HTMLButtonElement>) => void;
   loading: boolean;
@@ -19,28 +19,7 @@ const Modal = ({ onClick, loading }: ModalProps) => {
             onClick={onClick}
             className="bg-emerald-400 px-5 py-2 rounded-sm m-auto hover:shadow-lg flex items-center"
           >
-            {loading && (
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            )}
+            {loading && <img src={CircleSpin} alt="" />}
             Começar
           </button>
         </div>
